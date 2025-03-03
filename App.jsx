e5ae1932368518949e12ce1845b9674514f22595
study8practice <156129782+study8practice@users.noreply.github.com>
2025-03-03 19:19:25 -0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from "./components/Card";
+
+const App = () => {
+
+  return (
+    <section>
+      <Card />
+    </section>
+  );
+};
+
+export default App;
